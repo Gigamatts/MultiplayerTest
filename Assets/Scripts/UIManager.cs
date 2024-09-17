306596446ef3119fa28e5be33a7fbf4d287e7857
Ryann1908 <59339990+RyannThi@users.noreply.github.com>
2024-09-17 12:07:46 -0300

    BUI

--- Assets/Scripts/UIManager.cs
+++ Assets/Scripts/UIManager.cs
@@ -3,6 +3,9 @@ using System.Collections.Generic;
 using Unity.Netcode;
 using UnityEngine;
 using TMPro;
+using Unity.Netcode.Transports.UTP;
+using UnityEngine.UI;
+using System;
 
 
 public class UIManager : NetworkBehaviour
@@ -17,36 +20,64 @@ public class UIManager : NetworkBehaviour
     private NetworkVariable<int> playersNum = new NetworkVariable<int>(0, NetworkVariableReadPermission.Everyone);
     public static UIManager instance;
 
+    [SerializeField]
+    private UnityTransport netTransport;
+
+    [SerializeField]
+    private TMP_InputField ipField;
+
+    [SerializeField]
+    private TMP_InputField portField;
+
     // Start is called before the first frame update
     void Start()
     {
         instance = this;
+        NetworkManager.Singleton.ConnectionApprovalCallback += ClientApprovalCheck;
+
+        NetworkManager.Singleton.OnClientConnectedCallback += OnClientConnected;
+
+        StartCoroutine(ClientCounter());
+    }
+
+    private IEnumerator ClientCounter()
+    {
+        while(true)
+        {
+            if (IsServer)
+            {
+                playersNum.Value = NetworkManager.Singleton.ConnectedClients.Count;
+            }
+            clientsCounter.text = playersNum.Value.ToString();
+            yield return new WaitForSeconds(0.5f);
+        }
     }
 
     // Update is called once per frame
     void Update()
     {
-        clientsCounter.text = playersNum.Value.ToString();
-        if (!IsOwner) return;
-        if (IsServer)
-        {
-            playersNum.Value = NetworkManager.Singleton.ConnectedClients.Count;
-        }
+        
     }
     public void StartHost()
     {
+        Debug.Log("START HOST BUTTON");
+        netTransport.ConnectionData.Address = ipField.text;
+        netTransport.ConnectionData.Port = (ushort)Int32.Parse(portField.text);
         NetworkManager.Singleton.StartHost();
-        
     }
     public void StartClient()
     {
+        Debug.Log("START CLIENT BUTTON");
+        netTransport.ConnectionData.Address = ipField.text;
+        netTransport.ConnectionData.Port = (ushort)Int32.Parse(portField.text);
         NetworkManager.Singleton.StartClient();
     }
+
 
     [ServerRpc]
     public void StartSeekerServerRpc()
     {
-        GameObject seeker = Instantiate(seekerPrefab, new Vector3(Random.Range(-5, 5), 0.5f, Random.Range(-5, 5)),transform.rotation);
+        GameObject seeker = Instantiate(seekerPrefab, new Vector3(UnityEngine.Random.Range(-5, 5), 0.5f, UnityEngine.Random.Range(-5, 5)),transform.rotation);
         NetworkObject networkObject = seeker.GetComponent<NetworkObject>();
         if (networkObject != null)
         {
@@ -56,5 +87,38 @@ public class UIManager : NetworkBehaviour
     public void UpdateScore(int score)
     {
         UIManager.instance.scoreCounter.text = score.ToString();
+    }
+    
+    private async void OnClientConnected(ulong clientId)
+
+    {
+
+        if (clientId == NetworkManager.Singleton.LocalClientId) //if the connected client is our instance we don't need to acknowledge that connection.
+
+        {
+
+            Debug.Log("Host Connected as Client");
+
+            return;
+
+        }
+
+        Debug.Log("Client {clientId} Connected!");
+
+    }
+
+    private void ClientApprovalCheck(NetworkManager.ConnectionApprovalRequest request, NetworkManager.ConnectionApprovalResponse response) //this only runs on the host/server
+
+    {
+
+        Debug.Log("ApprovalCheck Requested");
+
+        response.Approved = true;
+
+        response.CreatePlayerObject = true;
+
+        // The Prefab hash value of the NetworkPrefab, if null the default NetworkManager player Prefab is used
+        response.PlayerPrefabHash = null;
+
     }
 }
